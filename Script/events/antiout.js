fc05cfc8702e87f717c35285a0631f35a30949b1
himelbd007 <www.nuralom7799@gmail.com>
2025-11-27 20:05:12 +0600

    Update antiout.js

--- Script/events/antiout.js
+++ Script/events/antiout.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antiout",
  eventType: ["log:unsubscribe"],
  version: "0.0.1",
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+ credits: "𝐇𝐈𝐌𝐄𝐋 𝐂𝐇𝐀𝐓 𝐁𝐎𝐓",
  description: "Listen events"
 };
 
