c55a2ee39964054b59ab296e360466e4d9548fb4
himelbd007 <www.nuralom7799@gmail.com>
2025-11-27 19:05:37 +0600

    Update random.js

--- Script/commands/random.js
+++ Script/commands/random.js
@@ -2,7 +2,7 @@ module.exports.config = {
  'name': "random",
  'version': "11.9.7",
  'hasPermission': 0,
- 'credits': "Shaon Ahmed",
+ 'credits': "𝐇𝐈𝐌𝐄𝐋",
  'description': "random love story video",
  'commandCategory': "video",
  'usages': "random",
